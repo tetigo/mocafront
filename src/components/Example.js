7437146ea2ae1694ca9a63abf91525c998004e6a
Tiago Mendes <tetigo@gmail.com>
2023-03-15 02:18:06 -0300

    fix:example box size

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,7 +1,7 @@
 
 const Example = () => {
     return (
-        <ul style={{marginTop: '-08.5rem', marginLeft:'25.5rem', marginRight:'45.5rem', border:'1px solid lightgray'}}>
+        <ul style={{marginTop: '-08.5rem', marginLeft:'25.5rem', marginRight:'20.5rem', border:'1px solid lightgray'}}>
                 <li>firstName: James</li>
                 <li>lastName: Smith</li>
                 <li>cardNumber: 5141576321679082</li>
