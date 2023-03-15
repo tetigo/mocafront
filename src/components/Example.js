cc75c65e4e32683fdfd84aca56f86ba963c4c381
Tiago Mendes <tetigo@gmail.com>
2023-03-15 06:46:38 -0300

    fix:box decoration

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,7 +1,12 @@
+import React from "react"
+
 
 const Example = () => {
     return (
-        <ul style={{marginTop: '-08.5rem', marginLeft:'25.5rem', marginRight:'20.5rem', border:'1px solid lightgray'}}>
+        <React.Fragment>
+        <ul style={{marginTop: '-08.5rem', marginLeft:'25.5rem', marginRight:'20.5rem', textDecoration: 'none', listStyle: 'none'}}>
+                <li>Database examples to use:</li>
+                <br/>
                 <li>firstName: James</li>
                 <li>lastName: Smith</li>
                 <li>cardNumber: 5141576321679082</li>
@@ -11,7 +16,8 @@ const Example = () => {
                 <li>lastName: Bean</li>
                 <li>cardNumber: 5141776321679088</li>
                 <li>pin: 4321</li>
-            </ul>
+        </ul>
+        </React.Fragment>
     )
 }
 
